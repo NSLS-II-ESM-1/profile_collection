d119c57dc8c33dbd690bfd345a948a503c638112
ESM Operator <xf21id1@xf21id1-ws1.cs.nsls2.local>
2017-03-23 13:12:41 -0400

    FIX: Include 'hdf5' in read_attrs of area detectors instances.

--- startup/30-detectors.py
+++ startup/30-detectors.py
@@ -72,75 +72,52 @@ class MyDetector(SingleTrigger, AreaDetector):
 Diag1_CamH = MyDetector('XF:21IDA-BI{Diag:1-Cam:H}', name='Diag1_CamH')
 Diag1_CamH.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam01/'
 Diag1_CamH.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam01/'
-Diag1_CamH.read_attrs = ['hdf5']
-Diag1_CamH.hdf5.read_attrs = []
-
 
 Diag1_CamV = MyDetector('XF:21IDA-BI{Diag:1-Cam:V}', name='Diag1_CamV')
 Diag1_CamV.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam02/'
 Diag1_CamV.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam02/'
-Diag1_CamV.read_attrs = ['hdf5']
-Diag1_CamV.hdf5.read_attrs = []
-
 
 Lock23A_CamEA3_1 = MyDetector('XF:21IDC-BI{BT:B2-Diag:B2_1}', name='Lock23A_CamEA3_1')
 Lock23A_CamEA3_1.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam03/'
 Lock23A_CamEA3_1.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam03/'
-Lock23A_CamEA3_1.read_attrs = ['hdf5']
-Lock23A_CamEA3_1.hdf5.read_attrs = []
 
 Lock14A_CamEA4_1 = MyDetector('XF:21IDD-BI{Lock1:4A-Cam:EA4_1}', name='Lock14A_CamEA4_1')
 Lock14A_CamEA4_1.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam04/'
 Lock14A_CamEA4_1.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam04/'
-Lock14A_CamEA4_1.read_attrs = ['hdf5']
-Lock14A_CamEA4_1.hdf5.read_attrs = []
 
 Prep2A_CamEA2_1 = MyDetector('XF:21IDD-BI{Prep:2A-Cam:EA2_1}', name='Prep2A_CamEA2_1')
 Prep2A_CamEA2_1.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam05/'
 Prep2A_CamEA2_1.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam05/'
-Prep2A_CamEA2_1.read_attrs = ['hdf5']
-Prep2A_CamEA2_1.hdf5.read_attrs = []
 
 Mir3_Cam10_U_1 = MyDetector('XF:21IDB-BI{Mir:3-Cam:10_U_1}', name='Mir3_Cam10_U_1')
 Mir3_Cam10_U_1.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam06/'
 Mir3_Cam10_U_1.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam06/'
-Mir3_Cam10_U_1.read_attrs = ['hdf5']
-Mir3_Cam10_U_1.hdf5.read_attrs = []
 
 BC1_Diag1_U_1 = MyDetector('XF:21IDA-BI{BC:1-Diag:1_U_1}', name='BC1_Diag1_U_1')
 BC1_Diag1_U_1.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam07/'
 BC1_Diag1_U_1.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam07/'
-BC1_Diag1_U_1.read_attrs = ['hdf5']
-BC1_Diag1_U_1.hdf5.read_attrs = []
 
 BTA2_DiagA2_1 = MyDetector('XF:21IDC-BI{BT:A2-Diag:A2_1}', name='BTA2_DiagA2_1')
 BTA2_DiagA2_1.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam08/'
 BTA2_DiagA2_1.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam08/'
-BTA2_DiagA2_1.read_attrs = ['hdf5']
-BTA2_DiagA2_1.hdf5.read_attrs = []
 
 BTB2_DiagB2_1 = MyDetector('XF:21IDC-BI{BT:B2-Diag:B2_1}', name='B2BT_DiagB2_1')
 BTB2_DiagB2_1.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam09/'
 BTB2_DiagB2_1.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam09/'
-BTB2_DiagB2_1.read_attrs = ['hdf5']
-BTB2_DiagB2_1.hdf5.read_attrs = []
 
 Anal1A_Camlens = MyDetector('XF:21IDD-BI{Anal:1A-Cam:lens}', name='Anal1A_Camlens')
 Anal1A_Camlens.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam10/'
 Anal1A_Camlens.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam10/'
-Anal1A_Camlens.read_attrs = ['hdf5']
-Anal1A_Camlens.hdf5.read_attrs = []
 
 Anal1A_Cambeam = MyDetector('XF:21IDD-BI{Anal:1A-Cam:beam}', name='Anal1A_Cambeam')
 Anal1A_Cambeam.hdf5.write_path_template = '/direct/XF21ID1/image_files/cam11/'
 Anal1A_Cambeam.hdf5.read_path_template = '/direct/XF21ID1/image_files/cam11/'
-Anal1A_Cambeam.read_attrs = ['hdf5']
-Anal1A_Cambeam.hdf5.read_attrs = []
 
 
 all_standard_pros = [Diag1_CamH,Diag1_CamV,Lock23A_CamEA3_1,Lock14A_CamEA4_1,Prep2A_CamEA2_1,Mir3_Cam10_U_1,BC1_Diag1_U_1,BTA2_DiagA2_1,BTB2_DiagB2_1,Anal1A_Camlens,Anal1A_Cambeam]
 for camera in all_standard_pros:
-    camera.read_attrs = ['stats1', 'stats2','stats3','stats4','stats5']
+    camera.read_attrs = ['stats1', 'stats2','stats3','stats4','stats5', 'hdf5']
+    camera.hdf5.read_attrs = []
     # camera.tiff.read_attrs = []  # leaving just the 'image'
     for stats_name in ['stats1', 'stats2','stats3','stats4','stats5']:
         stats_plugin = getattr(camera, stats_name)
