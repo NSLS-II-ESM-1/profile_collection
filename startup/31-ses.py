7549ad32ef4cc120045122c0217f7e9a56784407
John Sinsheimer <jsinsheimer@bnl.gov>
2020-11-02 09:24:20 -0500

    Updated 31-ses.py to have write_mode

--- startup/31-ses.py
+++ startup/31-ses.py
@@ -11,6 +11,15 @@ class SES(Device):
     width_en_sp = Component(EpicsSignal, ':width_en_SP')
     set_start = Component(EpicsSignal, ':request')
     done = Component(EpicsSignalRO, ':done')
+    iterations = Component(EpicsSignal, ':iterations_SP')
+    excitation_en = Component(EpicsSignal, ':excitation_en_SP')
+    steptime = Component(EpicsSignal, ':steptime_SP')
+    acq_mode = Component(EpicsSignal, ':acq_mode_SP')
+    pass_en = Component(EpicsSignal, ':pass_en_SP')
+    lens_mode  = Component(EpicsSignal, ':lens_mode_SP')
+    en_step = Component(EpicsSignal, ':en_step_SP')
+    write_mode = Component(EpicsSignal, ':write_mode')
+
 
     def trigger(self):
         """
@@ -30,5 +39,25 @@ class SES(Device):
         # And return the Status object, which the caller can use to
         # tell when the action is complete.
         return status
+
+    def read_params(self):
+        """
+        Read the existing parameters in SES into PVs
+        """
+        status = DeviceStatus(self)
+        # Wire up a callback that will mark the status object as finished
+        # when we see the state flip from "acquiring" to "not acquiring"---
+        # that is, a negative edge.
+        def callback(old_value, value, **kwargs):
+            if old_value == 0 and value == 1:
+                status._finished()
+                self.done.clear_sub(callback)
+
+        self.done.subscribe(callback, run=False)
+        self.set_start.put(2)
+        # And return the Status object, which the caller can use to
+        # tell when the action is complete.
+        return status
+
     
 ses = SES('XF21ID1-ES-SES', name='ses')
