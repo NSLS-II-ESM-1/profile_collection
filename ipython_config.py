120c5f025283f5d7d1cb3525c36cd22eec58d78e
ESM Operator <xf21id1@xf21id1-ws3.nsls2.bnl.local>
2021-07-19 11:02:13 -0400

    IPython configuration for Olog

--- ipython_config.py
+++ ipython_config.py
@@ -1,6 +1,8 @@
 c = get_config()
 c.StoreMagics.autorestore = True
-c.InteractiveShellApp.extensions = ['pyOlog.cli.ipy']
+import os
+if os.getenv("BS_ENABLE_OLOG_CALLBACK", 1) == 1:
+    c.InteractiveShellApp.extensions = ['pyOlog.cli.ipy']
 c.TerminalIPythonApp.log_datefmt = '%Y-%m-%d %H:%M:%S'
 c.TerminalInteractiveShell.show_rewritten_input = True
 # c.TerminalInteractiveShell.autocall = 2
