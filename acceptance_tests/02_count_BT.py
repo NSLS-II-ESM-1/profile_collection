45381d529b25c260453d4bd20fc3f337937d0e93
maffettone <phillip.maffettone@gmail.com>
2022-04-19 12:46:49 -0400

    Remove tests using undeclared motors and detectors

--- acceptance_tests/02_count_BT.py
+++
@@ -1,6 +0,0 @@
-from bluesky.plans import count
-from bluesky.callbacks import LiveTable
-
-
-assert BTA2_DiagA2_1.cam.connected
-RE(count([BTA2_DiagA2_1]), LiveTable([BTA2_DiagA2_1]))
--- acceptance_tests/03_scan_BT.py
+++
@@ -1,6 +0,0 @@
-from bluesky.plans import relative_scan
-from bluesky.callbacks import LiveTable
-
-
-RE(relative_scan([BTA2_DiagA2_1], BTA2_diag.trans, -0.1, 0.1, 5),
-   LiveTable([BTA2_diag.trans, BTA2_DiagA2_1]))
