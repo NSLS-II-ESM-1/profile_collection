f8761a3700e19cd3140fc5dd3f92c7da052f5ff1
ESM Operator <xf21id1@xf21id1-ws7.nsls2.bnl.local>
2025-04-11 10:36:16 -0400

    Verified redis_url and redis_prefix work

--- startup/01-base.py
+++ startup/01-base.py
@@ -11,8 +11,8 @@ EpicsSignalBase.set_defaults(connection_timeout=10)
 nslsii.configure_base(get_ipython().user_ns,
                       'arpes',
                       publish_documents_with_kafka=True,
-                      redis_prefix="arpes-", # TODO: Verify this is correct
-                      redis_url="info.esm.nsls2.bnl.gov") # TODO: Verify this is correct
+                      redis_prefix="arpes-",
+                      redis_url="info.esm.nsls2.bnl.gov")
 
 # Set ipython startup dir variable (used in some modules):
 PROFILE_STARTUP_PATH = os.path.abspath(get_ipython().profile_dir.startup_dir)
